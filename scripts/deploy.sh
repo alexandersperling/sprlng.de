eec6223f1daea29e17a71063c6cf6f6a69cbb840
Alexander Sperling <alexander@reinelt.de.com>
2019-06-24 19:09:57 +0000

    updated vita link, gitlab logo, ci

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
+DOMAIN=$1
 chmod 600 temp_id_rsa
 mkdir /root/.ssh
 ssh-keyscan sprlng.de > /root/.ssh/known_hosts
-ssh -i temp_id_rsa yolped@sprlng.de "rm -r /var/www/sprlng.de/*" || true
+ssh -i temp_id_rsa yolped@sprlng.de "rm -r /var/www/$DOMAIN/*" || true
 echo "########## Deploying ${CI_COMMIT_TAG} to prod ##########"
-scp -r -i temp_id_rsa ./src yolped@sprlng.de:/var/www/sprlng.de
+scp -r -i temp_id_rsa ./src "yolped@sprlng.de:/var/www/$DOMAIN"
 rm -rf temp_id_rsa
