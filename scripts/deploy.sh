a53a23d3672bcb390baa6d894dd83671eceffc5a
alexandersperling <alexander@reinelt.de.com>
2019-06-24 16:01:19 +0200

    use SSH_PRIVATE_KEY

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
+echo "{SSH_PRIVATE_KEY}" > temp_id_rsa
+chmod 600 temp_id_rsa
 mkdir /root/.ssh
 ssh-keyscan sprlng.de > /root/.ssh/known_hosts
-echo "{SSH_KEY}" > temp_id_rsa
-chmod 600 temp_id_rsa
 ssh -i temp_id_rsa yolped@sprlng.de "rm -r /var/www/sprlng.de/*" || true
 echo "########## Deploying ${CI_COMMIT_TAG} to prod ##########"
 scp -r -i temp_id_rsa ./src yolped@sprlng.de:/var/www/sprlng.de
