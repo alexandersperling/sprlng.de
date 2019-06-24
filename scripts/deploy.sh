1e20e691ec86663924391136dabed6c6e620302f
alexandersperling <alexander@reinelt.de.com>
2019-06-24 15:56:02 +0200

    different approach to create ssh key

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,9 @@
 
 mkdir /root/.ssh
 ssh-keyscan sprlng.de > /root/.ssh/known_hosts
-chmod 600 "${SSH_KEY}"
-ssh -i "${SSH_KEY}" yolped@sprlng.de "rm -r /var/www/sprlng.de/*" || true
+echo "{SSH_KEY}" > temp_id_rsa
+chmod 600 temp_id_rsa
+ssh -i temp_id_rsa yolped@sprlng.de "rm -r /var/www/sprlng.de/*" || true
 echo "########## Deploying ${CI_COMMIT_TAG} to prod ##########"
-scp -r -i "${SSH_KEY}" ./src yolped@sprlng.de:/var/www/sprlng.de
+scp -r -i temp_id_rsa ./src yolped@sprlng.de:/var/www/sprlng.de
+rm -rf temp_id_rsa
