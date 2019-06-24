cb15251f785414a94fe11678998fdf760c1a10e4
Alexander Sperling <alexander@reinelt.de.com>
2019-06-24 13:09:30 +0000

    deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-VERSION=$1
-OLD_VERSION=$2
-
-echo "${SSH_KEY}" > temp_rsa
-chmod 400 temp_rsa
 echo "########## Deploying ${CI_COMMIT_TAG} to prod ##########"
-scp -i temp_rsa yolped@sprlng.de:/home/yolped/sprlng.de
-ssh -i temp_rsa "rm -rf /var/www/sprlng.de/$OLD_VERSION && cp -R sprlng.de /var/www/sprlng.de/$VERSION"
-
+ssh -i "${SSH_KEY}" "rm -r /var/www/sprlng.de/*" || true
+scp -r -i "${SSH_KEY}" ./src yolped@sprlng.de:/var/www/sprlng.de
